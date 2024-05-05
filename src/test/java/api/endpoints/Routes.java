a7dfb8b15ef37f436d16f72d8f2f2eebfe65b55a
gsanjb <gaurav.sanjay-bagul@capgemini.com>
2024-05-06 00:35:32 +0530

    this is my first commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,22 @@
+package api.endpoints;
+
+public class Routes {
+
+public static String base_url  ="https://petstore.swagger.io/v2";
+ 	
+ 	//User Model
+    public static String post_url  = base_url+"/user";
+    public static String get_url=base_url+"/user/{username}";
+    public static String update_url=base_url+"/user/{username}";
+    public static String delete_url=base_url+"/user/{username}";
+     
+    
+    //Store Model
+    	//Here you can add Store url's
+    
+    
+    //Pet Model
+    	//Here you can add Pet url's
+
+	
+}
